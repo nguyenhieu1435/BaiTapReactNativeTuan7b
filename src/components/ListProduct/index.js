2a7a6af68704caec3b44519bb9536f2ffcc1fa97
Nguyễn Hiếu <hieurio12@gmail.com>
2023-10-22 19:12:23 +0700

    initial project, and completed GetStarted Page

---
+++ src/components/ListProduct/index.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+
+
+const DATAS = [
+    {
+        
+    }
+]
+
+export default function ListProduct() {
+
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
